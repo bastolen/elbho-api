7137273c21778d7aa6270a5bfa43009f8cd7246d
Bas Tolen <bas@bastolen.nl>
2019-12-07 17:58:26 +0100

    Added get appointment call

--- src/service/appointment.service.ts
+++ src/service/appointment.service.ts
@@ -1,3 +1,26 @@
-class AppointmentService { }
+import * as async from 'async';
+import { Appointment } from '../model/appointment';
+
+class AppointmentService {
+  static getAppointmentsForFilter(filterObject, cb) {
+    async.waterfall(
+      [
+        callback => Appointment.find(filterObject, callback).lean(),
+        (result, callback) => {
+          if (!result) {
+            return callback('not found');
+          }
+          return callback(undefined, result);
+        },
+      ],
+      cb
+    );
+  }
+
+  static addAppointment(appointment, cb) {
+    const newAppointment = new Appointment(appointment);
+    newAppointment.save(cb);
+  }
+}
 
 export { AppointmentService };
