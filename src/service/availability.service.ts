1ec06f827f72f948e6fb31f03612ea9e4e41a87a
Bas Tolen <bas@bastolen.nl>
2020-01-04 13:31:53 +0100

    Remove availability if start and end is the same

--- src/service/availability.service.ts
+++ src/service/availability.service.ts
@@ -16,40 +16,50 @@ class AvailabilityService {
           return callback(null);
         }
 
-        let dateObject;
+        let dateObject: Date;
         if (date && new Date(date).toString() !== 'Invalid Date') {
           dateObject = new Date(date);
         } else {
           return callback(null);
         }
 
-        let startObject;
+        let startObject: Date;
         if (start && new Date(start).toString() !== 'Invalid Date') {
           startObject = new Date(start);
         } else {
           return callback(null);
         }
 
-        let endObject;
+        let endObject: Date;
         if (end && new Date(end).toString() !== 'Invalid Date') {
           endObject = new Date(end);
         } else {
           return callback(null);
         }
 
-        const newAvailability = {
-          advisor: advisorId,
-          date: dateObject,
-          start: startObject,
-          end: endObject,
-        };
+        if (startObject.getTime() === endObject.getTime()) {
+          Availability.findOneAndDelete(
+            {
+              advisor: advisorId,
+              date: dateObject,
+            },
+            callback
+          );
+        } else {
+          const newAvailability = {
+            advisor: advisorId,
+            date: dateObject,
+            start: startObject,
+            end: endObject,
+          };
 
-        Availability.updateOne(
-          { date: dateObject, advisor: advisorId },
-          newAvailability,
-          { upsert: true, setDefaultsOnInsert: true },
-          callback
-        );
+          Availability.updateOne(
+            { date: dateObject, advisor: advisorId },
+            newAvailability,
+            { upsert: true, setDefaultsOnInsert: true },
+            callback
+          );
+        }
       },
       cb
     );
